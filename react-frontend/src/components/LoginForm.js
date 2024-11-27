32fa48e54e45fd20accf0749ad029df8169f813d
Andrii Streblichenko <vojavy@gmail.com>
2024-11-27 21:58:49 +0100

    zolupa

--- react-frontend/src/components/LoginForm.js
+++ react-frontend/src/components/LoginForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { login } from '../../services/authService';
+import { login } from '../services/authService';
 import {
   Box,
   TextField,
--- react-frontend/src/components/RegistrationForm.js
+++ react-frontend/src/components/RegistrationForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { register } from '../../services/authService';
+import { register } from '../services/authService';
 import {
   Box,
   Container,
