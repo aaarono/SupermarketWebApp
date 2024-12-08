bb8d76f1b4e941ada1d04a279398b9870f832100
Andrii Streblichenko <vojavy@gmail.com>
2024-12-08 22:46:17 +0100

    back | TOTAL CLEANING

--- backend/src/main/java/com/bdas_dva/backend/Repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bdas_dva.backend.Repository;
-
-import com.bdas_dva.backend.Model.Product;
-import java.util.List;
-
-public interface ProductRepository {
-    List<Product> findByNameContainingIgnoreCase(String name);
-    List<Product> findByNameContainingIgnoreCaseAndCategory(String name, String category);
-}
